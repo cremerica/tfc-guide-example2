77bbedd6f637e594326a183175db2d7b1d508209
Cortex <team@getcortexapp.com>
2024-06-26 16:21:27 +0000

    Initial Commit

--- main.tf
+++ main.tf
@@ -27,5 +27,5 @@ resource "aws_instance" "ubuntu" {
 
   tags = {
     Name = var.instance_name
-  }
+  }resource "aws_instance" "ubuntu" {   ami           = data.aws_ami.ubuntu.id   instance_type = var.instance_type    tags = {     Name = var.instance_name   } }
 }
